662d7e01bab9c7bfef5b7b3108fb05e020f9b3d3
bwpedro <pwarmling0@gmail.com>
2020-09-08 08:36:37 -0300

    feat(parking): add new parking functions to lambda

--- functions/parking/main.js
+++ functions/parking/main.js
@@ -1,11 +1,24 @@
 const resolver = require('./resolver');
 
 exports.handler = async (event) => {
-	console.log('lambda started: ', event);
 	try {
 		switch (event.field) {
 			case 'insertParkingSpots':
 				return await resolver.insertParkingSpots(event.arguments);
+			case 'getSpotsByStatus':
+				return await resolver.getSpotsByStatus(event.arguments);
+			case 'requestSpot':
+				return await resolver.requestSpot(event.arguments);
+			case 'requestSuccess':
+				return await resolver.requestSuccess(event.arguments);
+			case 'requestFailed':
+				return await resolver.requestFailed(event.arguments);
+			case 'carArrived':
+				return await resolver.carArrived(event.arguments);
+			case 'carLeft':
+				return await resolver.carLeft(event.arguments);
+			case 'getHistory':
+				return await resolver.getHistory(event.arguments);
 			default:
 				throw new Error('Method not allowed');
 		}
--- functions/parking/resolver.js
+++ functions/parking/resolver.js
@@ -1,4 +1,5 @@
 const { dynamodb } = require('./utils');
+const { v4 } = require('uuid');
 
 const asyncInsertParkingSpot = async (spot) => {
 	return dynamodb.insertIntoTable(
@@ -7,6 +8,44 @@ const asyncInsertParkingSpot = async (spot) => {
 			...spot,
 		}
 	);
+};
+
+const getSpotsByStatus = async ({ status }) => {
+	const result = await dynamodb.getFromTable(
+		`${process.env.APP}-parking-${process.env.STAGE}`,
+		'status',
+		status
+	);
+
+	if (result) {
+		return result;
+	}
+
+	return null;
+};
+
+const getHistory = async ({ filter }) => {
+	let result = null;
+
+	if (filter.driverId) {
+		result = await dynamodb.getFromTable(
+			`${process.env.APP}-parking-history-${process.env.STAGE}`,
+			'driverId',
+			filter.driverId
+		);
+	} else if (filter.spot) {
+		result = await dynamodb.getFromTable(
+			`${process.env.APP}-parking-${process.env.STAGE}`,
+			'spot',
+			filter.spot
+		);
+	}
+
+	if (result) {
+		return result;
+	}
+
+	return null;
 };
 
 const insertParkingSpots = async (args) => {
@@ -35,6 +74,90 @@ const insertParkingSpots = async (args) => {
 	return requestResult.every((result) => result);
 };
 
+const updateParkingSpot = async (payload) => {
+	const result = await dynamodb.insertIntoTable(
+		`${process.env.APP}-parking-${process.env.STAGE}`,
+		payload
+	);
+
+	if (result) {
+		return payload;
+	}
+
+	return null;
+};
+
+const requestSpot = async (args) => {
+	const payload = {
+		...args.input,
+		status: 'IN_NEGOTIATION',
+	};
+
+	return updateParkingSpot(payload);
+};
+
+const requestSuccess = async (args) => {
+	const payload = {
+		...args.input,
+		status: 'WAITING_FOR_PARKING',
+	};
+
+	return updateParkingSpot(payload);
+};
+
+const clearSpot = async (args) => {
+	const payload = {
+		...args.input,
+		status: 'FREE',
+		driverId: null,
+		startTime: null,
+		endTime: null,
+		date: null,
+		price: null,
+	};
+
+	return updateParkingSpot(payload);
+};
+
+const requestFailed = async (args) => {
+	return clearSpot(args);
+};
+
+const carArrived = async (args) => {
+	const payload = {
+		...args.input,
+		status: 'BUSY',
+	};
+
+	if (await updateParkingSpot(payload)) {
+		const result = dynamodb.insertIntoTable(
+			`${process.env.APP}-parking-history-${process.env.STAGE}`,
+			{
+				uuid: v4(),
+				timestamp: Date.now(),
+				driverId: payload.driverId,
+				spot: payload.spot,
+				price: payload.price,
+			}
+		);
+
+		if (result) return payload;
+	}
+
+	return null;
+};
+
+const carLeft = async (args) => {
+	return clearSpot(args);
+};
+
 module.exports = {
 	insertParkingSpots,
+	getSpotsByStatus,
+	getHistory,
+	requestSpot,
+	requestSuccess,
+	requestFailed,
+	carArrived,
+	carLeft,
 };
