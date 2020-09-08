80a4e599dbec6cd55e3e82ddb6da45a3e6d81c15
bwpedro <pwarmling0@gmail.com>
2020-09-08 08:37:21 -0300

    feat(user): add new function to user lambda

--- functions/user/main.js
+++ functions/user/main.js
@@ -1,13 +1,14 @@
 const resolver = require('./resolver');
 
 exports.handler = async (event) => {
-	console.log('lambda started: ', event);
 	try {
 		switch (event.field) {
 			case 'insertUser':
 				return await resolver.insertUser(event.arguments);
 			case 'getUser':
 				return await resolver.getUser(event.arguments);
+			case 'insertVehicle':
+				return await resolver.insertVehicle(event.arguments);
 			default:
 				throw new Error('Method not allowed');
 		}
--- functions/user/resolver.js
+++ functions/user/resolver.js
@@ -1,3 +1,4 @@
+const diff = require('deep-diff');
 const { dynamodb } = require('./utils');
 
 const insertUser = async (args) => {
@@ -11,13 +12,11 @@ const insertUser = async (args) => {
 	);
 };
 
-const getUser = async (input) => {
-	const { cpf } = input;
-
+const getUser = async ({ driverId }) => {
 	const [result] = await dynamodb.getFromTable(
 		`${process.env.APP}-user-${process.env.STAGE}`,
-		'cpf',
-		cpf
+		'driverId',
+		driverId
 	);
 
 	if (result) return result;
@@ -25,7 +24,32 @@ const getUser = async (input) => {
 	return null;
 };
 
+const insertVehicle = async (args) => {
+	const { input } = args;
+
+	const user = await getUser(input);
+	const getDiff = diff(user.vehicles, input.plates);
+
+	if (!getDiff) return true;
+
+	const newVehicles = [];
+	diff(user.vehicles, input.plates).map((element) => {
+		if (element.kind === 'N' || element.kind === 'E') {
+			newVehicles.push(element.rhs);
+		}
+	});
+
+	return dynamodb.insertIntoTable(
+		`${process.env.APP}-user-${process.env.STAGE}`,
+		{
+			...user,
+			vehicles: [...newVehicles],
+		}
+	);
+};
+
 module.exports = {
 	insertUser,
 	getUser,
+	insertVehicle,
 };
